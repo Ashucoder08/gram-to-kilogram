3c3fde91fa4694d473c4733fcf66e00d2834e285
Ashucoder08 <115220295+Ashucoder08@users.noreply.github.com>
2022-10-13 22:20:55 +0530

    Add files via upload

---
+++ UnitA.java
@@ -0,0 +1,27 @@
+public class UnitA
+{
+	public static void main(String args[])
+	{
+		
+		//gram to kilogram
+		
+		double  g, kilo, gram;
+		g = 5;
+		System.out.println("The gram is:"+g);
+		kilo =g/1000;   
+		System.out.println("The gram convert kilogram is:"+kilo);
+		
+		 // kilogram to gram
+		
+		System.out.println("The kilogram is :"+g);
+		gram = g * 1000; 
+		System.out.println("The Kilogram convert to gram:"+gram);
+		
+		
+		 
+		
+		
+	}
+
+
+}
